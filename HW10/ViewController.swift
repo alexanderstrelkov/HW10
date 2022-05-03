290959a99f8a515b82047362ff4ed1170d73824f
Alex <wild6158@gmail.com>
2022-05-03 12:42:58 +0300

    Initial Commit

---
+++ HW10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10
+//
+//  Created by Alexandr Strelkov on 03.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
